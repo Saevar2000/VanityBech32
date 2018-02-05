b31fe4f9cf41f67b77b2ccfabfda62b0f9cc29da
Saevar2000 <saevar2000@gmail.com>
2018-02-05 16:45:29 +0000

    Clean up

--- VanityBech32/Program.cs
+++ VanityBech32/Program.cs
@@ -10,69 +10,39 @@ namespace VanityBech32
 {
     class Program
     {
-        static int i = 0;
+        int i = 0;
 
         static void Main(string[] args)
+        {
+            var p = new Program();
+            p.VanityGen();
+            Console.ReadLine();
+        }
+
+        public void VanityGen()
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            string search = "bc1qsaev";
+            string search = "bc1qsae"; 
             Key privateKey;
             BitcoinWitPubKeyAddress Bech32;
-            void VanityGen()
+
+            while (true)
             {
-                while (true)
+                i++;
+
+                privateKey = new Key(); // Generate a random private key.
+                Bech32 = privateKey.PubKey.GetSegwitAddress(Network.Main);
+
+                if (Bech32.ToString().IndexOf(search, System.StringComparison.Ordinal) != -1)
                 {
-                    i++;
-
-                    privateKey = new Key(); // Generate a random private key
-                    Bech32 = privateKey.PubKey.GetSegwitAddress(Network.Main);
-
-                    if (Bech32.ToString().IndexOf(search, System.StringComparison.Ordinal) != -1)
-                    {
-                        sw.Stop();
-                        Console.WriteLine($"Finished on {i} attempt and took a total of {sw.Elapsed} seconds");
-                        Console.WriteLine("Address: " + Bech32);
-                        Console.WriteLine("Private Key: " + privateKey.GetWif(Network.Main));
-                        Environment.Exit(0);
-                    }
+                    sw.Stop();
+                    Console.WriteLine($"Finished on {i} attempt and took a total of {sw.Elapsed} seconds");
+                    Console.WriteLine("Address: " + Bech32);
+                    Console.WriteLine("Private Key: " + privateKey.GetWif(Network.Main));
                 }
             }
-
-            int numOfThreads = 5;
-            Thread[] ts = new Thread[numOfThreads];
-            for (int i = 0; i < numOfThreads; i++)
-            {
-                ts[i] = new Thread(() =>
-                {
-                    VanityGen();
-                });
-                ts[i].Start();
-            }
-
-            int s5 = 0;
-            List<int> s60 = new List<int>();
-            void Benchmark(object state)
-            {
-                if (s60.Count > 12)
-                {
-                    s60.RemoveAt(0);
-                }
-                Console.WriteLine($"Attempts: {i} --- Speed 5s/60s: {(i - s5)} {s60.Sum() / 12} Time: {sw.Elapsed}");
-                s60.Add(i - s5);
-                s5 = i;
-            }
-
-            using (new Timer(Benchmark, null, 0, 5000))
-            {
-                Console.WriteLine("Started... (This might take a while)");
-                Thread.Sleep(TimeSpan.FromDays(1));
-            }
-
-            Console.ReadLine();
         }
-
-        
     }
 }
